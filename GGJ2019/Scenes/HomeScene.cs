3c3b3f267374315f28d8ba296e11ee9ef2548dd6
Andrew Napierkowski <andrew@fantasydata.com>
2019-01-26 18:27:58 -0500

    drew the house scene

---
+++ GGJ2019/Scenes/HomeScene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GGJ2019.Constants;
+using GGJ2019.Entities;
+using GGJ2019.Util.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nez;
+using Nez.Textures;
+using Nez.Tiled;
+using GGJ2019.Util;
+
+namespace GGJ2019.Scenes
+{
+    public class HomeScene : Scene
+    {
+    }
+}
